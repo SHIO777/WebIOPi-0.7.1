518565d66a1ac6b1242dda74e1441519f06b4768
SHIO777 <shiotatsu1015@gmail.com>
2021-02-18 10:09:25 +0900

    executed patch file

--- python/webiopi/utils/thread.py
+++ python/webiopi/utils/thread.py
@@ -33,14 +33,14 @@ def stop(signum=0, frame=None):
             task.stop()
                 
 
-def runLoop(func=None, async=False):
+def runLoop(func=None, l_async=False):
     global RUNNING
     RUNNING = True
     signal.signal(signal.SIGINT, stop)
     signal.signal(signal.SIGTERM, stop)
 
     if func != None:
-        if async:
+        if l_async:
             TASKS.append(Task(func, True))
         else:
             while RUNNING:
